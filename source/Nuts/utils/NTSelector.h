7667608a3f3c73a05d3e7e3c30e34758f4f6a40b
zm <missingurchin@gmail.com>
2023-02-21 18:46:24 +0800

    New config.c for constants

--- source/Nuts/utils/NTSelector.h
+++
@@ -1,4 +0,0 @@
-typedef struct __SEL {
-    int x;
-    char *(*call)(struct __SEL *str, int y);
-} SEL;
--- source/main.c
+++ source/main.c
@@ -10,221 +10,93 @@
  *
  ********************************************************************************************/
 
-#include "raylib.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <switch.h>
+#include "NTButton.h"
+#include "raylib.h"
 
-#define FRAMES_COUNT 10
-#define BAT_FRAMES_COUNT 3
+#define FPS 60
 
-// from left to right
-void BreakDown2Images(Image src_image, Image *frames, int count,
-                      Rectangle src_rect) {
-  for (int i = 0; i <= count; i++) {
-    frames[i] = GenImageColor(src_rect.width, src_rect.height, BLANK);
-    ImageDraw(
-        &frames[i], src_image,
-        (Rectangle){i * src_rect.width, 0, src_rect.width, src_rect.height},
-        (Rectangle){0, 0, src_rect.width, src_rect.height}, WHITE);
-    ImageResize(&frames[i], 200, 200);
-  }
-}
+int main(void)
+{
+    // Init the gamepad for reading all controllers
+    PadState pad;
+    padInitializeAny(&pad);
+    hidInitializeTouchScreen();
 
-int main(void) {
-  // Init the gamepad for reading all controllers
-  PadState pad;
-  padInitializeAny(&pad);
-  hidInitializeTouchScreen();
+    // Init romfs if needed
+    romfsInit();
 
-  // Init romfs if needed
-  romfsInit();
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-  // Initialization
-  //--------------------------------------------------------------------------------------
-  const int screenWidth = 1280;
-  const int screenHeight = 720;
+    InitWindow(screenWidth, screenHeight, "nXt pixel dungeon");
 
-  InitWindow(screenWidth, screenHeight, "pixel dungeon");
+    InitAudioDevice();
+    SetTargetFPS(FPS);
+    SetTraceLogLevel(LOG_WARNING);
 
-  InitAudioDevice();
-  SetTargetFPS(20);
-  SetRandomSeed(1);
-  SetTraceLogLevel(LOG_WARNING);
+    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context
+    // is required)
 
-  // NOTE: Textures MUST be loaded after Window initialization (OpenGL context
-  // is required)
-  /* Texture2D texture = */
-  /*     LoadTexture("romfs:/resources/raylib_logo.png"); // Texture loading */
-  /* Music bgm = LoadMusicStream("romfs:/resources/background.ogg"); */
-  Image avator = LoadImage("romfs:/assets/sprites/bee.png");
-  Image avator_bat = LoadImage("romfs:/assets/sprites/bat.png");
+    Music bgm = LoadMusicStream("romfs:/resources/background.ogg");
+    Sound fx_click = LoadSound("romfs:/assets/sounds/click.mp3");
 
-  int rate = 1;
-  Image frames[FRAMES_COUNT + 1];
-  Image bat_frames[BAT_FRAMES_COUNT + 1];
-  /* ImageResizeNN(&avator, avator.width * rate, avator.height * rate); */
+    Texture2D button =
+        LoadTexture("romfs:/resources/button.png");  // Load button texture
 
-  /* for (int i = 0; i <= FRAMES_COUNT; i++) { */
-  /*   frames[i] = GenImageColor(16, 16, BLANK); */
-  /*   ImageDraw(&frames[i], avator, (Rectangle){i * 16, 0, 16, 16}, */
-  /*             (Rectangle){0, 0, 16, 16}, WHITE); */
-  /*   ImageResize(&frames[i], 200, 200); */
-  /* } */
-  BreakDown2Images(avator, frames, FRAMES_COUNT, (Rectangle){0, 0, 16, 16});
-  BreakDown2Images(avator_bat, bat_frames, BAT_FRAMES_COUNT,
-                   (Rectangle){0, 0, 15, 15});
+    float frameHeight = (float) button.height / NUM_FRAMES;
+    Rectangle sourceRec = {0, 0, (float) button.width, frameHeight};
+    Rectangle btn2Bounds = {600, 500, button.width, frameHeight};
 
-  UnloadImage(avator);
-  UnloadImage(avator_bat);
+    NTButton *btn = initButtonWith(button, sourceRec, btn2Bounds, fx_click,
+                                   "3 test action like class");
 
-  Music bgm = LoadMusicStream("romfs:/resources/bgm.mp3");
-  bgm.looping = true;
-  PlayMusicStream(bgm);
-  Image img = GenImageColor(screenWidth, screenHeight, BLANK);
-  int x = 0;
-  int y = 0;
-  int hue = 0;
-  int fps_count = 0;
-  int step = 0;
-  int bat_step = 0;
+    bgm.looping = true;
+    PlayMusicStream(bgm);
 
-  //---------------------------------------------------------------------------------------
-  const char message[128] =
-      "This sample illustrates a text writing\nanimation effect! Check it out!";
-  int frameCount = 0;
+    //---------------------------------------------------------------------------------------
 
-  Rectangle des_rec = (Rectangle){0, 0, 16, 16}; // dest
-  Vector2 origin = (Vector2){0.0f, 0.0f};       // poistion on source
+    // Main game loop
+    while (!WindowShouldClose())  // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+        padUpdate(&pad);
+        u64 kDown = padGetButtonsDown(&pad);
+        if (kDown & HidNpadButton_Plus) {
+            break;
+        }
 
-  NPatchInfo ninfo = {(Rectangle){0, 0, 15, 16}, // what you want to get
-                      0,                         // left
-                      0,                         // top
-                      0,                         // right
-                      0,                         // bottom
-                      NPATCH_NINE_PATCH};
-  s32 prev_touchcount = 0;
+        UpdateMusicStream(bgm);
 
-  // Main game loop
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
-    //----------------------------------------------------------------------------------
-    padUpdate(&pad);
-    u64 kDown = padGetButtonsDown(&pad);
-    if (kDown & HidNpadButton_Plus) {
-      break;
-    } else if (kDown & HidNpadButton_A) {
-      frameCount = 0;
-    } else if (kDown & HidNpadButton_Y) {
-      frameCount += 8;
-    } else {
-      frameCount++;
-    }
-    
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
 
-    HidTouchScreenState state = {0};
+        ClearBackground(WHITE);
+        btn->draw(btn);
 
-    HidAnalogStickState analog_stick_l = padGetStickPos(&pad, 0);
-    HidAnalogStickState analog_stick_r = padGetStickPos(&pad, 1);
+        EndDrawing();
 
-    UpdateMusicStream(bgm);
-
-    /* Texture texture = LoadTextureFromImage(img); */
-    Texture2D test_texture = LoadTexture("romfs:/assets/sprites/bat.png");
-    int rate = 10;
-    /* if (test_rec.width > 32) { */
-    /*   test_rec.width = 16; */
-    /*   test_rec.height = 16; */
-    /* } */
-    hidGetTouchScreenStates(&state, 1);
-    rate *= state.count;
-    if(rate == 0) rate = 1;
-    des_rec.width = 16 * rate;
-    des_rec.height = 16 * rate;
-
-    /* if (bat_step > 4){ */
-    /*     ninfo.source.x = 0; */
-    /*     bat_step= 0; */
-    /* } */
-    /* ninfo.source.x += 16; */
-    /* bat_step++; */
-
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
-    ClearBackground(WHITE);
-
-    if (hidGetTouchScreenStates(&state, 1)) {
-      if (state.count != prev_touchcount) {
-        prev_touchcount = state.count;
-        consoleClear();
-      }
-      for (s32 i = 0; i < state.count; i++) {
-        DrawCircle(state.touches[i].x, state.touches[i].y, 100.0f, RED);
-        DrawText(
-            TextFormat("[%d] x=%03d, y=%03d, diameter_x=%03d, diameter_y=%03d, "
-                       "rotation_angle=%03d\n",
-                       i, state.touches[i].x, state.touches[i].y,
-                       state.touches[i].diameter_x, state.touches[i].diameter_y,
-                       state.touches[i].rotation_angle),
-            40, 600, 20, BLACK);
-      }
+        //----------------------------------------------------------------------------------
     }
 
-    DrawText(TextSubtext(message, 0, frameCount / 10), 210, 160, 20, BLACK);
-    /* DrawText(TextFormat("step %d", step), 360, 370, 50, BLACK); */
-    DrawText(TextFormat("3;1H%04d; %04d", analog_stick_l.x, analog_stick_l.y),
-             360, 370, 50, BLACK);
-    DrawText(TextFormat("3;1H%04d; %04d", analog_stick_r.x, analog_stick_r.y),
-             360, 420, 50, BLACK);
-    /* DrawTexture(bee, 400, 400, WHITE); */
-    /* DrawTexture(bat, 500, 500, WHITE); */
-    /* DrawTexture(test_texture, 500, 550, WHITE); */
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
 
-    /* DrawTextureRec(test_texture,test_rec, (Vector2){500, 549}, WHITE); */
+    UnloadMusicStream(bgm);
+    UnloadTexture(button);
+    CloseWindow();  // Close window and OpenGL context
 
-    DrawTextureNPatch(test_texture, ninfo, des_rec, origin, 0.0f, WHITE);
-    /* ninfo.source.x += 16; */
-    /* des_rec.x += des_rec.width; */
-    /* DrawTextureNPatch(test_texture, ninfo, des_rec, origin, 0.0f, WHITE); */
-    /* ninfo.source.x += 16; */
-    /* des_rec.x += des_rec.width; */
-    /* DrawTextureNPatch(test_texture, ninfo, des_rec, origin, 0.0f, WHITE); */
-    /* ninfo.source.x += 16; */
-    /* des_rec.x += des_rec.width; */
-    /* DrawTextureNPatch(test_texture, ninfo, des_rec, origin, 0.0f, WHITE); */
-    /* DrawTexture(texture, 0, 0, WHITE); */
+    CloseAudioDevice();
+    romfsExit();  // Close romfs
+    //--------------------------------------------------------------------------------------
 
-    EndDrawing();
-
-    /* UnloadTexture(bee); // Texture unloading */
-    /* UnloadTexture(bat); // Texture unloading */
-    UnloadTexture(test_texture);
-    /* UnloadTexture(texture); // Texture unloading */
-    //----------------------------------------------------------------------------------
-  }
-
-  // De-Initialization
-  //--------------------------------------------------------------------------------------
-
-  for (int i = 0; i <= FRAMES_COUNT; i++) {
-    UnloadImage(frames[i]);
-  }
-
-  for (int i = 0; i <= BAT_FRAMES_COUNT; i++) {
-    UnloadImage(bat_frames[i]);
-  }
-
-  UnloadMusicStream(bgm);
-  UnloadImage(img);
-  CloseWindow(); // Close window and OpenGL context
-
-  CloseAudioDevice();
-  romfsExit(); // Close romfs
-  //--------------------------------------------------------------------------------------
-
-  return 0;
+    return 0;
 }
