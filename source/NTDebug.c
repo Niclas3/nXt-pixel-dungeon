a64910f5ebb748f61d33b6522ac9c02a00d97706
zm <missingurchin@gmail.com>
2023-03-05 11:42:10 +0800

    Add debugger function

---
+++ source/NTDebug.c
@@ -0,0 +1,63 @@
+#include "NTDebug.h"
+#include <switch.h>
+// void ShowLine(){
+//     DrawLine(but_dest.x, but_dest.y, but_dest.x+1000, but_dest.y, RED);
+// }
+
+void ShowRectInfo(Rectangle rect1)
+{
+    DrawText(TextFormat("x:%f y:%f w:%f h:%f", rect1.x, rect1.y, rect1.width,
+                        rect1.height),
+             200, 539, 20, RED);
+}
+
+void ShowVector2Info(Vector2 v)
+{
+    DrawText(TextFormat("x:%f y:%f", v.x, v.y), 200, 560, 20, GREEN);
+}
+
+void ShowRuler(bool is_h)
+{
+    HidTouchScreenState touch_state = {0};
+    Vector2 touchPoint = {0.0f, 0.0f};
+
+    hidGetTouchScreenStates(&touch_state, 1);
+    // Only load the newset touch point
+    if (touch_state.count != 0) {
+        touchPoint.x = touch_state.touches[touch_state.count - 1].x;
+        touchPoint.y = touch_state.touches[touch_state.count - 1].y;
+    }
+    int degree = 0;
+
+    Rectangle rec = {
+        .x = touchPoint.x, .y = touchPoint.y, .width = 600, .height = 100};
+
+    if (is_h) {
+        DrawRectanglePro(rec, (Vector2){0, 0}, degree, GREEN);
+        for (int i = 0; i < rec.width / 10; i++) {
+            int height = 10;
+            Vector2 s_pos = {.x = rec.x + i * 10, .y = rec.y};
+            Vector2 e_pos = {.x = rec.x + i * 10, .y = rec.y + height};
+            if (i % 10 == 0) {
+                e_pos.y += height;
+                DrawLineEx(s_pos, e_pos, 1.0, BLUE);
+            } else {
+                DrawLineEx(s_pos, e_pos, 1.0, RED);
+            }
+        }
+    } else {
+        degree = 90;
+        DrawRectanglePro(rec, (Vector2){0, 0}, degree, GREEN);
+        for (int i = 0; i < rec.width / 10; i++) {
+            int height = 10;
+            Vector2 s_pos = {.x = rec.x + i * 10, .y = rec.y};
+            Vector2 e_pos = {.x = rec.x + i * 10, .y = rec.y + height};
+            if (i % 10 == 0) {
+                e_pos.y += height;
+                DrawLineEx(s_pos, e_pos, 1.0, BLUE);
+            } else {
+                DrawLineEx(s_pos, e_pos, 1.0, RED);
+            }
+        }
+    }
+}
---
+++ source/NTDebug.h
@@ -0,0 +1,12 @@
+#ifndef NTDEBUG_H
+#define NTDEBUG_H
+#include "raylib.h"
+// void ShowLine(){
+//     DrawLine(but_dest.x, but_dest.y, but_dest.x+1000, but_dest.y, RED);
+// }
+
+void ShowRectInfo(Rectangle rect1);
+void ShowVector2Info(Vector2 rect1);
+void ShowRuler(bool is_h);
+
+#endif
