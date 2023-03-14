8c2f97fad28aab7979faa9b5e4fa895fa78badea
zm <missingurchin@gmail.com>
2023-03-14 10:22:12 +0800

    Empty Sprite model

---
+++ source/NTSprite.c
@@ -0,0 +1,9 @@
+#include "NTSprite.h"
+
+Vector2 icon_window = {17.0, 16.0};
+Vector2 mage_window = {12.0, 15.0};
+
+/* NTDrawTexture( */
+/*     mage, (Rectangle){x, y, mage_window.x, mage_window.y}, */
+/*     (Rectangle){1200, 200, mage_window.x * rate, mage_window.y * rate}); */
+
---
+++ source/NTSprite.h
@@ -0,0 +1,5 @@
+#ifndef NTSPRITE_H
+#define NTSPRITE_H
+#include <raylib.h>
+
+#endif
